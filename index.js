a7690cffbf1b75969c6f210b063a9f22bccadefd
atiqur rahman <abulbasharatiqur@gmail.com>
2024-07-06 22:04:27 +0600

    render use

--- index.js
+++ index.js
@@ -1,158 +1,158 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
-const cors = require('cors');
-const ObjectId = require('mongodb').ObjectId;
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// midleware 
+// midleware
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rwz2c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 // console.log(uri);
 
 async function run() {
-    try {
-        await client.connect();
-        console.log('Database connected successfully');
-        const database = client.db('airplane');
-        const productCollection = database.collection('products');
-        const reviewCollection = database.collection('reviews');
-        const buyerCollection = database.collection('buyers');
-        const userCollection = database.collection('users');
-        const orderCollection = database.collection('orders');
+  try {
+    await client.connect();
+    console.log("Database connected successfully");
+    const database = client.db("airplane");
+    const productCollection = database.collection("products");
+    const reviewCollection = database.collection("reviews");
+    const buyerCollection = database.collection("buyers");
+    const userCollection = database.collection("users");
+    const orderCollection = database.collection("orders");
 
-        //GET API
-        app.get('/products', async (req, res) => {
-            const cursor = productCollection.find({})
-            const products = await cursor.toArray();
-            res.json(products);
-        })
+    //GET API
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const products = await cursor.toArray();
+      res.json(products);
+    });
 
-        //POST API
-        app.post('/products', async (req, res) => {
-            const product = req.body;
-            // console.log('product',product);
-            const result = await productCollection.insertOne(product);
-            // console.log(result);
-            res.json(result);
-        })
+    //POST API
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      // console.log('product',product);
+      const result = await productCollection.insertOne(product);
+      // console.log(result);
+      res.json(result);
+    });
 
-        // DELETE API 
-        app.delete('/products/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await productCollection.deleteOne(query);
-            res.json(result);
-        })
+    // DELETE API
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.json(result);
+    });
 
-        // limit product
-        app.get('/limitProducts', async (req, res) => {
-            const cursor = productCollection.find({})
-            const products = await cursor.limit(6).toArray();
-            res.json(products);
-        })
+    // limit product
+    app.get("/limitProducts", async (req, res) => {
+      const cursor = productCollection.find({});
+      const products = await cursor.limit(6).toArray();
+      res.json(products);
+    });
 
-        //GET API
-        app.get('/reviews', async (req, res) => {
-            const cursor = reviewCollection.find({})
-            const reviews = await cursor.toArray();
-            res.json(reviews);
-        })
+    //GET API
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewCollection.find({});
+      const reviews = await cursor.toArray();
+      res.json(reviews);
+    });
 
-        // POST reviews 
-        app.post('/reviews', async (req, res) => {
-            const review = req.body;
-            // console.log('Hit the post API.', review);
-            const result = await reviewCollection.insertOne(review);
-            // console.log(result);
-            res.json(result);
-        })
+    // POST reviews
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      // console.log('Hit the post API.', review);
+      const result = await reviewCollection.insertOne(review);
+      // console.log(result);
+      res.json(result);
+    });
 
-        // POST places 
-        app.post('/buyers', async (req, res) => {
-            const buyer = req.body;
-            // console.log('Hit the post API.', buyer);
-            const result = await buyerCollection.insertOne(buyer);
-            // console.log(result);
-            res.json(result);
-        })
+    // POST places
+    app.post("/buyers", async (req, res) => {
+      const buyer = req.body;
+      // console.log('Hit the post API.', buyer);
+      const result = await buyerCollection.insertOne(buyer);
+      // console.log(result);
+      res.json(result);
+    });
 
-        // POST users 
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-            // console.log('Hit the post API.', user);
-            const result = await userCollection.insertOne(user);
-            // console.log(result);
-            res.json(result);
-        })
+    // POST users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      // console.log('Hit the post API.', user);
+      const result = await userCollection.insertOne(user);
+      // console.log(result);
+      res.json(result);
+    });
 
-        // POST orders
-        app.post('/orders', async (req, res) => {
-            const order = req.body;
-            const result = await orderCollection.insertOne(order);
-            res.json(result);
-        })
+    // POST orders
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.json(result);
+    });
 
-        //GET API
-        app.get('/orders', async (req, res) => {
-            let query = {};
-            const email = req.query.email;
-            if (email) {
-                query = { email: email };
-            }
-            const cursor = orderCollection.find(query);
-            const services = await cursor.toArray();
-            res.json(services);
-        })
+    //GET API
+    app.get("/orders", async (req, res) => {
+      let query = {};
+      const email = req.query.email;
+      if (email) {
+        query = { email: email };
+      }
+      const cursor = orderCollection.find(query);
+      const services = await cursor.toArray();
+      res.json(services);
+    });
 
+    // upsert
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updataDoc = { $set: user };
+      const result = await userCollection.updateOne(filter, updataDoc, options);
+      res.json(result);
+    });
 
-        // upsert
-        app.put('/users', async (req, res) => {
-            const user = req.body;
-            const filter = { email: user.email };
-            const options = { upsert: true };
-            const updataDoc = { $set: user };
-            const result = await userCollection.updateOne(filter, updataDoc, options);
-            res.json(result);
-        })
+    // PUT Admin
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
 
-        // PUT Admin
-        app.put('/users/admin', async (req, res) => {
-            const user = req.body;
-            const filter = { email: user.email };
-            const updateDoc = { $set: { role: 'admin' } };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.json(result);
-        })
-
-        //GET admin
-        app.get('/users/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email };
-            const user = await userCollection.findOne(query);
-            let isAdmin = false;
-            if (user?.role === 'admin') {
-                isAdmin = true;
-            }
-            res.json({ admin: isAdmin })
-        })
-
-    }
-    finally {
-        // await client.connect();
-    }
+    //GET admin
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+  } finally {
+    // await client.connect();
+  }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Airplane assignment server is running');
+app.get("/", (req, res) => {
+  res.send("Airplane assignment server is running");
 });
 
 app.listen(port, () => {
-    console.log("Server is running port", port);
-})
+  console.log("Server is running port", port);
+});
